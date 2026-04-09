83a793ccf6033a4b2745d07e61d5af59865f091a
RahulJangra79 <rjangra4015@gmail.com>
2026-04-09 19:05:44 +0530

    fixing jenkinsfile 16

--- app/index.js
+++ app/index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>CI/CD DevOps Project</title>
+      <title>CI/CD DevOps Project.</title>
       <style>
         * {
           margin: 0;
