5089f6a14b3c845218c3ae35fe4533154c9db6d1
RahulJangra79 <rjangra4015@gmail.com>
2026-04-13 14:24:26 +0530

    project completre

--- app/index.js
+++ app/index.js
@@ -1,13 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("🚀 CI/CD Pipeline with Docker, Jenkins, Terraform, Ansible Working!");
-// });
-
-// app.listen(3000, () => console.log("Server running on port 3000"));
-
-
 const express = require("express");
 const app = express();
 
